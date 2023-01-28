c92451f2cc9edadd295c7eee73d16948dbfd499b
joaovelozo <joaovelozo@gmail.com>
2023-01-27 21:38:03 -0300

    general_update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:i9/src/screens/auth/loginScreen.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:sqflite/sqflite.dart';
 
 void main() {
+  sqfliteFfiInit();
+  databaseFactory = databaseFactoryFfi;
   runApp(const MyApp());
 }
 
